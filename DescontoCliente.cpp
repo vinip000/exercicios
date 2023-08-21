7e45515a9b6499b02d6758bcf4bbdb5b97b5a660
vinip000 <112911506+vinip000@users.noreply.github.com>
2023-08-21 20:08:53 -0300

    Add files via upload

---
+++ DescontoCliente.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+float descontoBronze(float compra = 0){
+    float taxa = 0.05;
+    float desconto = 0;
+    return desconto = compra - compra*taxa;
+}
+float descontoPrata(float compra = 0){
+    float taxa = 0.1;
+    float desconto = 0;
+    return desconto = compra - compra*taxa;
+}
+
+float descontoOuro(float compra = 0){
+    float taxa = 0.15;
+    float desconto = 0;
+    return desconto = compra - compra*taxa;
+}
+
+int main(){
+    float compra;
+    cout<< "Digite o valor da compra: "<<endl;
+    cin>> compra;
+    if (compra >=100){
+    cout<< "bronze: "<<descontoBronze(compra);
+   }else if (compra>=200){
+    cout<< "Prata: "<< descontoPrata(compra);
+   }else{
+    cout<< "Ouro: "<< descontoOuro(compra);
+   }
+    return 0;
+}
